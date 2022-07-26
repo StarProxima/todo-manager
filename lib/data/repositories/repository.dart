8cd59829be4cd7522955106eac14cce36814c4ec
StarProxima <maks.prozorov1@yandex.ru>
2022-07-26 23:30:52 +0300

    Abstract Repository

---
+++ lib/data/repositories/repository.dart
@@ -0,0 +1,110 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+abstract class Repository {
+  static const int _countSecTimeOut = 10;
+  static const String _timeOutMessage = "Ошибка подключения к серверу";
+  static const String baseUrl = 'https://beta.mrdekk.ru/todobackend/';
+
+  const Repository();
+
+  Future<ResponseData> postRequest({
+    required String url,
+    required String parametrs,
+    required Map<String, String> headers,
+  }) async {
+    try {
+      http.Response response = await http
+          .post(
+            Uri.parse(baseUrl + url),
+            headers: headers,
+            body: parametrs,
+          )
+          .timeout(const Duration(seconds: _countSecTimeOut));
+
+      return ResponseData(
+        data: utf8.decode(response.bodyBytes),
+        status: response.statusCode,
+      );
+    } catch (e) {
+      return const ResponseData(data: _timeOutMessage);
+    }
+  }
+
+  Future<ResponseData> getRequest(
+    String url, {
+    Map<String, String>? headers,
+  }) async {
+    try {
+      http.Response response = await http
+          .get(
+            Uri.parse(baseUrl + url),
+            headers: headers,
+          )
+          .timeout(const Duration(seconds: _countSecTimeOut));
+
+      return ResponseData(
+        data: utf8.decode(response.bodyBytes),
+        status: response.statusCode,
+      );
+    } catch (e) {
+      return const ResponseData(data: _timeOutMessage);
+    }
+  }
+
+  Future<ResponseData> putRequest({
+    required String url,
+    required String parametrs,
+    required Map<String, String> headers,
+  }) async {
+    try {
+      http.Response response = await http
+          .put(
+            Uri.parse(baseUrl + url),
+            headers: headers,
+            body: parametrs,
+          )
+          .timeout(const Duration(seconds: _countSecTimeOut));
+
+      return ResponseData(
+        data: utf8.decode(response.bodyBytes),
+        status: response.statusCode,
+      );
+    } catch (e) {
+      return const ResponseData(data: _timeOutMessage);
+    }
+  }
+
+  Future<ResponseData> deleteRequest({
+    required String url,
+    required Map<String, String> headers,
+  }) async {
+    try {
+      http.Response response = await http
+          .delete(
+            Uri.parse(baseUrl + url),
+            headers: headers,
+          )
+          .timeout(const Duration(seconds: _countSecTimeOut));
+
+      return ResponseData(
+        data: utf8.decode(response.bodyBytes),
+        status: response.statusCode,
+      );
+    } catch (e) {
+      return const ResponseData(data: _timeOutMessage);
+    }
+  }
+}
+
+class ResponseData {
+  final String data;
+  final int status;
+  final bool isSuccesful;
+
+  const ResponseData({
+    required this.data,
+    this.status = 404,
+    this.isSuccesful = false,
+  });
+}
