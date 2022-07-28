519be54fecf6542b1d31e6265b1d75c687a46b95
StarProxima <maks.prozorov1@yandex.ru>
2022-07-29 02:16:00 +0300

    HomePage with debug  buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:todo_manager/data/models/task_model.dart';
-import 'package:todo_manager/data/repositories/task_repository.dart';
 import 'package:todo_manager/ui/pages/home_page.dart';
 
 void main() {
@@ -16,57 +15,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   Task? lastTask;
-
-  void getTaskList() async {
-    final tasks = await TaskRepository().getTasks();
-
-    lastTask = tasks.isNotEmpty ? tasks.last : null;
-    print(tasks.toString());
-  }
-
-  void addTask() async {
-    var response = await TaskRepository().addTask(
-      Task(
-        importance: Importance.low,
-        done: false,
-        text: 'way',
-      ),
-    );
-    print(response);
-  }
-
-  void editTask() async {
-    var response = await TaskRepository().editTask(
-      lastTask!.copyWith(
-        text: 'NOOO WAAAY',
-      ),
-    );
-    print(response);
-  }
-
-  void deleteTask() async {
-    var response = await TaskRepository().deleteTask(
-      lastTask!,
-    );
-    print(response);
-  }
-
-  void patchTasks() async {
-    final tasks = await TaskRepository().getTasks();
-    tasks.addAll([
-      Task(
-        importance: Importance.low,
-        done: false,
-        text: 'way',
-      ),
-      Task(
-        importance: Importance.low,
-        done: false,
-        text: 'way',
-      ),
-    ]);
-    print(await TaskRepository().patchTasks(tasks));
-  }
 
   @override
   Widget build(BuildContext context) {
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_manager/data/models/task_model.dart';
+import 'package:todo_manager/data/repositories/task_repository.dart';
 import 'package:todo_manager/ui/widgets/task_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,14 +11,85 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Task> tasks = [];
+  Future<void> getTasks() async {
+    tasks = await TaskRepository().getTasks();
+    setState(() {});
+    print(tasks.toString());
+  }
+
+  void addTask() async {
+    var response = await TaskRepository().addTask(
+      Task.random(),
+    );
+    await getTasks();
+    print(response);
+  }
+
+  void editTask() async {
+    if (tasks.isNotEmpty) {
+      var response = await TaskRepository()
+          .editTask(tasks.last.copyWith(text: 'Edited text <3'));
+      await getTasks();
+      print(response);
+    }
+  }
+
+  void deleteTask() async {
+    if (tasks.isNotEmpty) {
+      var response = await TaskRepository().deleteTask(tasks.last);
+      await getTasks();
+      print(response);
+    }
+  }
+
+  void patchTasks() async {
+    print(tasks);
+    tasks.addAll([]);
+    await TaskRepository().patchTasks(tasks);
+
+    await getTasks();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return TaskCard(task: Task.random());
-        },
+      body: Column(
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: getTasks,
+                child: const Text('getTasks'),
+              ),
+              ElevatedButton(
+                onPressed: addTask,
+                child: const Text('addTask'),
+              ),
+              ElevatedButton(
+                onPressed: editTask,
+                child: const Text('editTask'),
+              ),
+              ElevatedButton(
+                onPressed: deleteTask,
+                child: const Text('deleteTask'),
+              ),
+              ElevatedButton(
+                onPressed: patchTasks,
+                child: const Text('patchTasks'),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return TaskCard(task: tasks[index]);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
