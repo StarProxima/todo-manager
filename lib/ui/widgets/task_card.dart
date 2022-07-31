e937d54805346f8f415b1de4a6e92ebf60926c7d
StarProxima <maks.prozorov1@yandex.ru>
2022-07-31 05:12:08 +0300

    Dismissible TaskCard

--- lib/ui/widgets/task_card.dart
+++ lib/ui/widgets/task_card.dart
@@ -7,8 +7,10 @@ import 'package:todo_manager/ui/styles/app_theme.dart';
 import 'package:todo_manager/ui/widgets/task_checkbox.dart';
 
 class TaskCard extends StatefulWidget {
-  const TaskCard({Key? key, required this.task}) : super(key: key);
+  const TaskCard({Key? key, required this.task, this.onDelete})
+      : super(key: key);
 
+  final Function()? onDelete;
   final Task task;
   @override
   State<TaskCard> createState() => _TaskCardState();
@@ -16,100 +18,203 @@ class TaskCard extends StatefulWidget {
 
 class _TaskCardState extends State<TaskCard> {
   bool isDebug = false;
+
+  GlobalKey<AppDismissBackgroundState> dismissStartToEndKey =
+      GlobalKey<AppDismissBackgroundState>();
+
+  GlobalKey<AppDismissBackgroundState> dismissEndToStartKey =
+      GlobalKey<AppDismissBackgroundState>();
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        TaskCheckbox(
-          task: widget.task,
-          onChanged: (value) {
+    return Dismissible(
+      key: ValueKey(widget.task.id),
+      onDismissed: (_) {
+        widget.onDelete?.call();
+      },
+      confirmDismiss: (direction) async {
+        switch (direction) {
+          case DismissDirection.endToStart:
+            return true;
+
+          case DismissDirection.startToEnd:
             setState(() {
-              widget.task.done = value;
+              widget.task.done = true;
             });
-          },
+            break;
+
+          default:
+            break;
+        }
+        return false;
+      },
+      onUpdate: (details) {
+        //Ужас, помогите
+        if (details.direction == DismissDirection.startToEnd) {
+          dismissStartToEndKey.currentState?.setProgress(details.progress);
+        } else {
+          dismissEndToStartKey.currentState?.setProgress(details.progress);
+        }
+      },
+      background: Container(
+        color: AppColors.green,
+        child: Row(
+          children: [
+            AppDismissBackground(
+              key: dismissStartToEndKey,
+              direction: DismissDirection.startToEnd,
+              icon: const Icon(Icons.check, color: Colors.white),
+            ),
+          ],
         ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: Container(
-                  color: isDebug ? Colors.amber : null,
-                  child: RichText(
-                    text: TextSpan(
-                      style: widget.task.done
-                          ? AppTextStyle.crossedOut
-                          : Theme.of(context).textTheme.bodyMedium,
-                      children: [
-                        widget.task.importance != Importance.basic
-                            ? WidgetSpan(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(right: 3),
-                                  child: AppSvgIcons(
-                                    widget.task.importance ==
-                                            Importance.important
-                                        ? AppSvgIcon.important
-                                        : AppSvgIcon.low,
-                                    color: widget.task.done
-                                        ? AppTextStyle.crossedOut.color
-                                        : null,
-                                  ),
-                                ),
-                              )
-                            : const TextSpan(),
-                        TextSpan(
-                          text: widget.task.text,
-                          style: widget.task.done
-                              ? AppTextStyle.crossedOut
-                              : Theme.of(context).textTheme.bodyMedium,
-                        ),
-                      ],
-                    ),
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              ),
-              widget.task.deadline != null
-                  ? Padding(
-                      padding: const EdgeInsets.only(top: 4),
-                      child: Container(
-                        color: isDebug ? Colors.amber : null,
-                        child: Text(
-                          DateFormat('dd.MM.yyyy hh:mm')
-                              .format(widget.task.deadline!),
-                          style: Theme.of(context).textTheme.labelSmall,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    )
-                  : const SizedBox(),
-            ],
-          ),
+      ),
+      secondaryBackground: Container(
+        color: AppColors.red,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            AppDismissBackground(
+              key: dismissEndToStartKey,
+              direction: DismissDirection.endToStart,
+              icon: const Icon(Icons.delete, color: Colors.white),
+            ),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.only(top: 15, left: 14, right: 18),
-          child: IconButton(
-            onPressed: () {
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TaskCheckbox(
+            task: widget.task,
+            onChanged: (value) {
               setState(() {
-                isDebug = !isDebug;
+                widget.task.done = value;
               });
             },
-            splashRadius: 20,
-            padding: EdgeInsets.zero,
-            constraints: const BoxConstraints(),
-            icon: Container(
-              color: isDebug ? Colors.amber : null,
-              child: const Icon(
-                Icons.info_outline,
-                color: Color(0x4d000000),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: Container(
+                    color: isDebug ? Colors.amber : null,
+                    child: RichText(
+                      text: TextSpan(
+                        style: widget.task.done
+                            ? AppTextStyle.crossedOut
+                            : Theme.of(context).textTheme.bodyMedium,
+                        children: [
+                          widget.task.importance != Importance.basic
+                              ? WidgetSpan(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(right: 3),
+                                    child: AppSvgIcons(
+                                      widget.task.importance ==
+                                              Importance.important
+                                          ? AppSvgIcon.important
+                                          : AppSvgIcon.low,
+                                      color: widget.task.done
+                                          ? AppTextStyle.crossedOut.color
+                                          : null,
+                                    ),
+                                  ),
+                                )
+                              : const TextSpan(),
+                          TextSpan(
+                            text: widget.task.text,
+                            style: widget.task.done
+                                ? AppTextStyle.crossedOut
+                                : Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ),
+                widget.task.deadline != null
+                    ? Padding(
+                        padding: const EdgeInsets.only(top: 4),
+                        child: Container(
+                          color: isDebug ? Colors.amber : null,
+                          child: Text(
+                            DateFormat('dd.MM.yyyy hh:mm')
+                                .format(widget.task.deadline!),
+                            style: Theme.of(context).textTheme.labelSmall,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      )
+                    : const SizedBox(),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 15, left: 14, right: 18),
+            child: IconButton(
+              onPressed: () {
+                setState(() {
+                  isDebug = !isDebug;
+                });
+              },
+              splashRadius: 20,
+              padding: EdgeInsets.zero,
+              constraints: const BoxConstraints(),
+              icon: Container(
+                color: isDebug ? Colors.amber : null,
+                child: const Icon(
+                  Icons.info_outline,
+                  color: Color(0x4d000000),
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
+    );
+  }
+}
+
+class AppDismissBackground extends StatefulWidget {
+  const AppDismissBackground({
+    Key? key,
+    required this.direction,
+    required this.icon,
+  }) : super(key: key);
+
+  final DismissDirection direction;
+  final Widget icon;
+  @override
+  State<AppDismissBackground> createState() => AppDismissBackgroundState();
+}
+
+class AppDismissBackgroundState extends State<AppDismissBackground> {
+  void setProgress(double progress) {
+    setState(() {
+      padding = MediaQuery.of(context).size.width * progress - 48;
+    });
+  }
+
+  double padding = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: widget.direction == DismissDirection.startToEnd
+            ? padding > 24
+                ? padding
+                : 24
+            : 0,
+        right: widget.direction == DismissDirection.endToStart
+            ? padding > 24
+                ? padding
+                : 24
+            : 0,
+      ),
+      child: widget.icon,
     );
   }
 }
